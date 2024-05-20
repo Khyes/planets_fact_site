87474438e608fe7b7ff656e07f660c1a3db79cdd
alexy piche <alexyautumn@gmail.com>
2024-05-20 11:54:38 -0400

    optimize and comment js code

--- planet.js
+++ planet.js
@@ -1,264 +1,275 @@
+// create variable for info buttons
+const overviewBtn = document.getElementById('overview-btn');
+const internalBtn = document.getElementById('internal-btn');
+const geologyBtn = document.getElementById('geology-btn');
 
-var overviewBtn = document.getElementById('overview-btn');
-    var internalBtn = document.getElementById('internal-btn');
-    var geologyBtn = document.getElementById('geology-btn');
+// create variable for info mobile buttons
+const overviewBtnNav = document.getElementById('overview-btn-nav');
+const internalBtnNav = document.getElementById('internal-btn-nav');
+const geologyBtnNav = document.getElementById('geology-btn-nav');
 
-var overviewBtnNav = document.getElementById('overview-btn-nav');
-    var internalBtnNav = document.getElementById('internal-btn-nav');
-    var geologyBtnNav = document.getElementById('geology-btn-nav');
+// create variable for info sections
+const overviewInfo = document.getElementById('overview-info');
+const internalInfo = document.getElementById('internal-info');
+const geologyInfo = document.getElementById('geology-info');
 
-    var overviewInfo = document.getElementById('overview-info');
-    var internalInfo = document.getElementById('internal-info');
-    var geologyInfo = document.getElementById('geology-info');
-
-    overviewBtn.addEventListener('click', function() {
-        gsap.from(internalInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(internalInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
-        gsap.from(geologyInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(geologyInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
-
-          overviewBtn.classList.add('active');
-          internalBtn.classList.remove('active');
-          geologyBtn.classList.remove('active');
-
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'block';
-            internalInfo.style.display = 'none';
-            geologyInfo.style.display = 'none';
-            gsap.from(overviewInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(overviewInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+// animation when overview button is clicked
+overviewBtn.addEventListener('click', function() {
+    //hide the two other info section
+    gsap.from(internalInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(internalInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(geologyInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(geologyInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
-    internalBtn.addEventListener('click', function() {
+    //change the active class on the buttons
+    overviewBtn.classList.add('active');
+    internalBtn.classList.remove('active');
+    geologyBtn.classList.remove('active');
+
+    //show the overview info section after the other ones where hidden
+    setTimeout(() => {
+        overviewInfo.style.display = 'block';
+        internalInfo.style.display = 'none';
+        geologyInfo.style.display = 'none';
         gsap.from(overviewInfo, {
-            opacity: 1, 
+            opacity: 0,
             duration: 1,
-          });
+        });
         gsap.to(overviewInfo, {
-            opacity: 0, 
+            opacity: 1,
             duration: 1,
-          });
-        gsap.from(geologyInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(geologyInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
+        });
 
-          overviewBtn.classList.remove('active');
-          internalBtn.classList.add('active');
-          geologyBtn.classList.remove('active');
+    }, 1000);
+});
 
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'none';
-            internalInfo.style.display = 'block';
-            geologyInfo.style.display = 'none';
-
-            gsap.from(internalInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(internalInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+// animation when internal button is clicked
+internalBtn.addEventListener('click', function() {
+  //hide the two other info section
+    gsap.from(overviewInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(overviewInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(geologyInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(geologyInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
-    geologyBtn.addEventListener('click', function() {
-        gsap.from(overviewInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(overviewInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
+    //change the active class on the buttons
+    overviewBtn.classList.remove('active');
+    internalBtn.classList.add('active');
+    geologyBtn.classList.remove('active');
+
+    //show the internal info section after the other ones where hidden
+    setTimeout(() => {
+
+        overviewInfo.style.display = 'none';
+        internalInfo.style.display = 'block';
+        geologyInfo.style.display = 'none';
+
         gsap.from(internalInfo, {
-            opacity: 1, 
+            opacity: 0,
             duration: 1,
-          });
+        });
         gsap.to(internalInfo, {
-            opacity: 0, 
+            opacity: 1,
             duration: 1,
-          });
+        });
 
-          overviewBtn.classList.remove('active');
-          internalBtn.classList.remove('active');
-          geologyBtn.classList.add('active');
+    }, 1000);
+});
 
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'none';
-            internalInfo.style.display = 'none';
-            geologyInfo.style.display = 'block';
-            gsap.from(geologyInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(geologyInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+// animation when geology button is clicked
+geologyBtn.addEventListener('click', function() {
+   //hide the two other info sections
+    gsap.from(overviewInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(overviewInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(internalInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(internalInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
-    gsap.from(".stats", {
-        scrollTrigger: '.stats',
-        innerText: 0,
-        duration: 5,
-        snap : {
-           innerText: 0.1
-        }
-      });
+    //change the active class on the buttons
+    overviewBtn.classList.remove('active');
+    internalBtn.classList.remove('active');
+    geologyBtn.classList.add('active');
 
+    //show the geology info section after the other ones where hidden
+    setTimeout(() => {
 
-    //   MOBILE
-
-    overviewBtnNav.addEventListener('click', function() {
-        gsap.from(internalInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(internalInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
+        overviewInfo.style.display = 'none';
+        internalInfo.style.display = 'none';
+        geologyInfo.style.display = 'block';
         gsap.from(geologyInfo, {
-            opacity: 1, 
+            opacity: 0,
             duration: 1,
-          });
+        });
         gsap.to(geologyInfo, {
-            opacity: 0, 
+            opacity: 1,
             duration: 1,
-          });
+        });
 
-          
-          overviewBtnNav.classList.add('active-mobile');
-          internalBtnNav.classList.remove('active-mobile');
-          geologyBtnNav.classList.remove('active-mobile');
+    }, 1000);
+});
+
+//stats count up animation
+gsap.from(".stats", {
+    scrollTrigger: '.stats',
+    innerText: 0,
+    duration: 5,
+    snap: {
+        innerText: 0.1
+    }
+});
 
 
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'block';
-            internalInfo.style.display = 'none';
-            geologyInfo.style.display = 'none';
-            gsap.from(overviewInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(overviewInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+//   MOBILE
+overviewBtnNav.addEventListener('click', function() {
+    gsap.from(internalInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(internalInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(geologyInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(geologyInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
-    internalBtnNav.addEventListener('click', function() {
+
+    overviewBtnNav.classList.add('active-mobile');
+    internalBtnNav.classList.remove('active-mobile');
+    geologyBtnNav.classList.remove('active-mobile');
+
+
+    setTimeout(() => {
+
+        overviewInfo.style.display = 'block';
+        internalInfo.style.display = 'none';
+        geologyInfo.style.display = 'none';
         gsap.from(overviewInfo, {
-            opacity: 1, 
+            opacity: 0,
             duration: 1,
-          });
+        });
         gsap.to(overviewInfo, {
-            opacity: 0, 
+            opacity: 1,
             duration: 1,
-          });
-        gsap.from(geologyInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(geologyInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
+        });
 
-          overviewBtnNav.classList.remove('active-mobile');
-          internalBtnNav.classList.add('active-mobile');
-          geologyBtnNav.classList.remove('active-mobile');
+    }, 1000);
+});
 
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'none';
-            internalInfo.style.display = 'block';
-            geologyInfo.style.display = 'none';
-
-            gsap.from(internalInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(internalInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+internalBtnNav.addEventListener('click', function() {
+    gsap.from(overviewInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(overviewInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(geologyInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(geologyInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
-    geologyBtnNav.addEventListener('click', function() {
-        gsap.from(overviewInfo, {
-            opacity: 1, 
-            duration: 1,
-          });
-        gsap.to(overviewInfo, {
-            opacity: 0, 
-            duration: 1,
-          });
+    overviewBtnNav.classList.remove('active-mobile');
+    internalBtnNav.classList.add('active-mobile');
+    geologyBtnNav.classList.remove('active-mobile');
+
+    setTimeout(() => {
+
+        overviewInfo.style.display = 'none';
+        internalInfo.style.display = 'block';
+        geologyInfo.style.display = 'none';
+
         gsap.from(internalInfo, {
-            opacity: 1, 
+            opacity: 0,
             duration: 1,
-          });
+        });
         gsap.to(internalInfo, {
-            opacity: 0, 
+            opacity: 1,
             duration: 1,
-          });
+        });
 
-          overviewBtnNav.classList.remove('active-mobile');
-          internalBtnNav.classList.remove('active-mobile');
-          geologyBtnNav.classList.add('active-mobile');
+    }, 1000);
+});
 
-        setTimeout(() => { 
-
-            overviewInfo.style.display = 'none';
-            internalInfo.style.display = 'none';
-            geologyInfo.style.display = 'block';
-            gsap.from(geologyInfo, {
-                opacity: 0, 
-                duration: 1,
-              });
-            gsap.to(geologyInfo, {
-                opacity: 1, 
-                duration: 1,
-              });
-            
-         },1000);
+geologyBtnNav.addEventListener('click', function() {
+    gsap.from(overviewInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(overviewInfo, {
+        opacity: 0,
+        duration: 1,
+    });
+    gsap.from(internalInfo, {
+        opacity: 1,
+        duration: 1,
+    });
+    gsap.to(internalInfo, {
+        opacity: 0,
+        duration: 1,
     });
 
+    overviewBtnNav.classList.remove('active-mobile');
+    internalBtnNav.classList.remove('active-mobile');
+    geologyBtnNav.classList.add('active-mobile');
 
+    setTimeout(() => {
+
+        overviewInfo.style.display = 'none';
+        internalInfo.style.display = 'none';
+        geologyInfo.style.display = 'block';
+        gsap.from(geologyInfo, {
+            opacity: 0,
+            duration: 1,
+        });
+        gsap.to(geologyInfo, {
+            opacity: 1,
+            duration: 1,
+        });
+
+    }, 1000);
+});
--- script.js
+++ script.js
@@ -1,85 +1,5 @@
-function myFunction() {
-    var x = document.getElementById("mobile-nav");
-    const close = document.getElementById('close')
-    if (x.style.display === "block") {
-      gsap.from(x, {
-        x: 0, // background-color
-        duration: 2,
-      });
-      gsap.to(x, {
-        x: 500,
-        duration: 2,
-      });
 
-      setTimeout(function() {
-        x.style.display = "none";
-      },2000);
-
-      gsap.from(close, {
-        opacity: 1,
-        duration: 0.5,
-      })
-
-      gsap.to(close, {
-        opacity: 0,
-        duration: 0.5,
-      })
-      setTimeout(function() {
-        close.classList.add('fa-bars');
-        close.classList.remove('fa-xmark');
-
-        gsap.from(close, {
-          opacity: 0,
-          duration: 0.5,
-        })
-  
-        gsap.to(close, {
-          opacity: 1,
-          duration: 0.5,
-        })
-
-      }, 500);
-
-    
-    } else {
-
-      gsap.from(x, {
-        x: 500, // background-color
-        duration: 2,
-      });
-      gsap.to(x, {
-        x: 0,
-        duration: 2,
-      });
-      x.style.display = "block";
-
-      gsap.from(close, {
-        opacity: 1,
-        duration: 0.5,
-      })
-
-      gsap.to(close, {
-        opacity: 0,
-        duration: 0.5,
-      })
-      setTimeout(function() {
-        close.classList.remove('fa-bars');
-        close.classList.add('fa-xmark');
-
-        gsap.from(close, {
-          opacity: 0,
-          duration: 0.5,
-        })
-  
-        gsap.to(close, {
-          opacity: 1,
-          duration: 0.5,
-        })
-
-      }, 500);
-    }
-  }
-
+// ANIMATIONS ON LOAD
 
 const body = document.querySelector('body')
 const nav = document.querySelector('nav')
@@ -90,71 +10,170 @@ const fromLeft = document.getElementsByClassName('from-left')
 
 
 window.onload = function() {
+  // wait 500 ms before display the page, just to be sure everything is loaded.
   setTimeout(() => {
-
     document.getElementById("loading").style.display = "none";
-    // gsap code here!
+    
+    //body fade in
     gsap.from(body, {
-        opacity: 0, // background-color
+        opacity: 0, 
         duration: 2,
-      });
+    });
     gsap.to(body, {
-        opacity: 1, // background-color
+        opacity: 1, 
         duration: 2,
-      });
+    });
+
+    //nav fade in
     gsap.from(nav, {
-        y: -100, // background-color
+        y: -100, 
         duration: 2,
-      });
+    });
     gsap.to(nav, {
-        y: 0, // background-color
+        y: 0, 
         duration: 2,
-      });
+    });
 
+    // fade in from bottom
     gsap.from(fromBottom, {
-        y: 100, // background-color
+        y: 100, 
         duration: 2,
-      });
+    });
     gsap.to(fromBottom, {
-        y: 0, // background-color
+        y: 0, 
         duration: 2,
-      });
+    });
 
+    // fade in from top
     gsap.from(fromTop, {
-        y: -100, // background-color
+        y: -100, 
         duration: 2,
-      });
+    });
     gsap.to(fromTop, {
-        y: 0, // background-color
+        y: 0, 
         duration: 2,
-      });
+    });
 
+    // fade in from right
     gsap.from(fromRight, {
-        x: 100, // background-color
+        x: 100, 
         y: 100,
         duration: 2,
-      });
+    });
     gsap.to(fromRight, {
-        y: 0, // background-color
+        y: 0, 
         x: 0,
         duration: 2,
-      });
+    });
 
+    // fade in from left
     gsap.from(fromLeft, {
-        x: -100, // background-color
+        x: -100, 
         y: 100,
         duration: 2,
-      });
+    });
     gsap.to(fromLeft, {
-        y: 0, // background-color
+        y: 0, 
         x: 0,
         duration: 2,
-      });
-  },1000);
-
-
+    });
+  },500);
 };
 
+// MOBILE NAV OPEN/CLOSE ANIMATION
+
+function myFunction() {
+  //select mobile-nav and close/open button
+  var x = document.getElementById("mobile-nav");
+  const close = document.getElementById('close')
+  // when "close" is clicked
+  if (x.style.display === "block") {
+    //move out nav
+    gsap.from(x, {
+      x: 0, // 
+      duration: 2,
+    });
+    gsap.to(x, {
+      x: 500,
+      duration: 2,
+    });
+
+    //fade out close button
+    gsap.from(close, {
+      opacity: 1,
+      duration: 0.5,
+    })
+
+    gsap.to(close, {
+      opacity: 0,
+      duration: 0.5,
+    })
+
+    //wait for the animations to be done before displaying to none
+    setTimeout(function() {
+      x.style.display = "none";
+    },2000);
+
+    //wait for the previous animation to be done before showing the open button
+    setTimeout(function() {
+      close.classList.add('fa-bars');
+      close.classList.remove('fa-xmark');
+
+      gsap.from(close, {
+        opacity: 0,
+        duration: 0.5,
+      })
+
+      gsap.to(close, {
+        opacity: 1,
+        duration: 0.5,
+      })
+
+    }, 500);
+
+  // when "open" is clicked
+  } else {
+
+    //move in nav
+    gsap.from(x, {
+      x: 500, 
+      duration: 2,
+    });
+    gsap.to(x, {
+      x: 0,
+      duration: 2,
+    });
+    x.style.display = "block";
+
+    //hide "open" button
+    gsap.from(close, {
+      opacity: 1,
+      duration: 0.5,
+    })
+
+    gsap.to(close, {
+      opacity: 0,
+      duration: 0.5,
+    })
+
+    //wait for the previous animation to be done before showing the close button
+    setTimeout(function() {
+      close.classList.remove('fa-bars');
+      close.classList.add('fa-xmark');
+
+      gsap.from(close, {
+        opacity: 0,
+        duration: 0.5,
+      })
+
+      gsap.to(close, {
+        opacity: 1,
+        duration: 0.5,
+      })
+
+    }, 500);
+  }
+}
     
 
 // Select all <a> elements and iterate over them to add event listeners
@@ -163,7 +182,7 @@ document.querySelectorAll("a").forEach(function(link) {
         // Prevent the default action of the link (i.e., navigating to the target page)
         event.preventDefault();
 
-        // Perform GSAP animation on the body element
+        // fade out the body
         gsap.from(document.body, {
             opacity: 1, // starting opacity
             duration: 1,
@@ -173,55 +192,59 @@ document.querySelectorAll("a").forEach(function(link) {
             duration: 1,
         });
 
+        //fade in nav
         gsap.from(nav, {
-            y: 0, // background-color
+            y: 0, 
             duration: 1,
-          });
+        });
         gsap.to(nav, {
-            y: -100, // background-color
+            y: -100, 
             duration: 1,
-          });
+        });
 
-          gsap.from(fromBottom, {
-            y: 0, // background-color
-            duration: 2,
-          });
+        //fade in from bottom
+        gsap.from(fromBottom, {
+          y: 0, 
+          duration: 2,
+        });
         gsap.to(fromBottom, {
-            y: 100, // background-color
-            duration: 2,
-          });
+          y: 100, 
+          duration: 2,
+        });
 
-          gsap.from(fromTop, {
-            y: 0, // background-color
-            duration: 2,
-          });
+        //fade in from top
+        gsap.from(fromTop, {
+          y: 0, 
+          duration: 2,
+        });
         gsap.to(fromTop, {
-            y: -100, // background-color
-            duration: 2,
-          });
+          y: -100, 
+          duration: 2,
+        });
     
-    
+        //fade in from right
         gsap.from(fromRight, {
-            x: 0, // background-color
+            x: 0, 
             y: 0,
             duration: 2,
-          });
+        });
         gsap.to(fromRight, {
-            y: 100, // background-color
+            y: 100, 
             x: 100,
             duration: 2,
-          });
+        });
     
+        //fade in from left
         gsap.from(fromLeft, {
-            x: 0, // background-color
+            x: 0, 
             y: 0,
             duration: 2,
-          });
+        });
         gsap.to(fromLeft, {
-            y: 100, // background-color
+            y: 100, 
             x: -100,
             duration: 2,
-          });
+        });
 
         // Wait for 2 seconds (2000 milliseconds) before redirecting to the target page
         setTimeout(function() {
@@ -233,5 +256,3 @@ document.querySelectorAll("a").forEach(function(link) {
         }, 1000); // 2000 milliseconds = 2 seconds
     });
 });
-
-// mobile nav animation
